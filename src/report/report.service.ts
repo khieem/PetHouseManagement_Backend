0e2c802b046eafcc1b4dfa3b3d210a99600e832b
Trần Quang Nguyên <tranquangnguyen2811@gmail.com>
2021-08-08 21:09:38 +0700

    Revert "Tqnguyen"

--- src/report/report.service.ts
+++ src/report/report.service.ts
@@ -1,7 +1,7 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { ClinicService } from 'src/clinic/clinic.service';
-import { OK, res } from 'src/constants';
+import { OK } from 'src/constants';
 import { Report } from 'src/entity/report.entity';
 import { ImageService } from 'src/image/image.service';
 import { PetService } from 'src/pet/pet.service';
@@ -30,16 +30,35 @@ export class ReportService {
 	async getReportbyClinic(id: number) {
 		const found = await this.clinicService.get(id);
 		if (!found) throw new NotFoundException();
-		return res(found.reports);
+		return found.reports;
 	}
 
 	async create(dto: CreateReportDto) {
 		let rp = this.reportDb.create();
-		const { images, note, petId, clinicId } = dto;
+		const {
+			images,
+			note,
+			petId,
+			clinicId,
+			weight,
+			overall,
+			allergies,
+			vaccines,
+			surgeries,
+			diagnosis,
+			prescription,
+		} = dto;
 		for (const img in images) {
 			await this.imageService.create(img, rp);
 		}
 		rp.note = note;
+		rp.weight = weight;
+		rp.overall = overall;
+		rp.allergies = allergies;
+		rp.vaccines = vaccines;
+		rp.surgeries = surgeries;
+		rp.diagnosis = diagnosis;
+		rp.prescription = prescription;
 		rp.pet = await this.petService.getbyId(petId);
 		rp.clinic = await this.clinicService.get(clinicId);
 		await this.reportDb.save(rp);
