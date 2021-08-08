4bb5c85853cedb139e76900ea15cf797f75b1b4c
Trần Quang Nguyên <tranquangnguyen2811@gmail.com>
2021-08-09 00:32:03 +0700

    add PATCH to cors

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
 	app.enableCors({
 		origin: '*',
-		methods: 'GET, PUT, POST, DELETE',
+		methods: 'GET, PUT, PATCH, POST, DELETE',
 		allowedHeaders: 'Content-Type, Authorization',
 	});
 	await app.listen(process.env.PORT || 3000);
