b62052c10b2d2fa7b3773d4bd16184bb3745c06f
Trần Quang Nguyên <tranquangnguyen2811@gmail.com>
2021-08-08 17:57:41 +0700

    update cors

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,11 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
 	const app = await NestFactory.create(AppModule);
-	app.enableCors();
+	app.enableCors({
+		origin: '*',
+		methods: 'GET, PUT, POST, DELETE',
+		allowedHeaders: 'Content-Type, Authorization',
+	});
 	await app.listen(process.env.PORT || 3000);
 }
 bootstrap();
