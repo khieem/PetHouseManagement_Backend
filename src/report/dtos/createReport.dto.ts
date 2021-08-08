e16c56e5fefa8c9d9e80c4fc408acee2d1d6c97d
HeoSolji <49313463+HeoSolji@users.noreply.github.com>
2021-08-08 11:40:47 +0700

    migration_report

--- src/report/dtos/createReport.dto.ts
+++ src/report/dtos/createReport.dto.ts
@@ -3,4 +3,11 @@ export class CreateReportDto {
 	images: string[];
 	petId: number;
 	clinicId: number;
+	weight: string;
+	overall: string;
+	allergies: string;
+	vaccines: string;
+	surgeries: string;
+	diagnosis: string;
+	prescription: string;
 }
